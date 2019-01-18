2207560b89a096f5d11053a72c75e46c8734169a
ViolinLee <mechleechan@outlook.com>
2019-01-18 11:23:08 +0800

    add 2d grid mapping algorithm solution

--- exercises/2d_grid_mapping/occGridMapping.m
+++ exercises/2d_grid_mapping/occGridMapping.m
@@ -21,8 +21,9 @@ lo_free = param.lo_free;
 lo_max = param.lo_max;
 lo_min = param.lo_min;
 
-N = size(pose,2); % Col number:measurement times
-n = size(scanAngles); % Row number:divisions number of single measurement
+N = size(pose,2); % Col number:measurement times = 3701
+n = size(scanAngles); % Row number:divisions number of single measurement --> number of rays = 1081
+robotGrid = [];
 
 for j = 1:N % for each time, (up to N=3701 times)
     % Vectorization can avoids double for loops, so 'n' is not used
@@ -30,17 +31,44 @@ for j = 1:N % for each time, (up to N=3701 times)
     % Find grids hit by the rays (in the gird map coordinate)
     x_occ = ranges(:, j) .* cos(scanAngles + pose(3, j)) + pose(1, j);
     y_occ = -1 * ranges(:, j) .* sin(scanAngles + pose(3, j)) + pose(2, j);
-    car_id = [ceil(pose(1, j) * myResol), ceil(pose(2, j) * myResol)] + myorigin;
+    car_id = ceil([pose(1, j), pose(2, j)] * myResol + myorigin');
     
     % Find occupied-measurement cells and free-measurement cells
-    occ_id = [ceil(x_occ * myResol), ceil(y_occ * myResol)] + myorigin;
-    occ_id_del = occ_id(:,2)<1 | occ_id(:,1)<1 | occ_id(:,2)>param.size(1) | occ_id(:,1)>param.size(2); % Crop Map
-    occ_id(occ_id_del) = [];
+    occ_id = ceil([x_occ, y_occ] * myResol + repmat(myorigin', [n, 1]));
+    %occ_id = sub2ind(size(myMap), occ_id(:,2),occ_id(:,1)); % convert to 1d index
+    myMap(occ_id) = myMap(occ_id) + lo_occ;
+    
+    free = [];
+    for cnt = 1:n
+        % Compute ID of free cells
+        [free_x, free_y] = bresenham(car_id(1),car_id(2),occ_id(cnt,1),occ_id(cnt,2));  
+        % Convert to 1d index
+        free = [free;sub2ind(size(myMap), free_y, free_x)];
+        
+        myMap(occ_id(cnt,2),occ_id(cnt,1)) = myMap(occ_id(cnt,2),occ_id(cnt,1)) + lo_occ;
+    end
+    
+    %[free_x, free_y] = bresenham(repmat(car_id(1),[n, 1]), repmat(car_id(2),[n, 1]), occ_id(:,1), occ_id(:,2));
+    %free = sub2ind(size(myMap),free_y,free_x); % convert to 1d index
+    
 
     % Update the log-odds
-    % Saturate the log-odd values
-    % Visualize the map as needed
+    myMap(free) = myMap(free) - lo_free;
     
+    % Saturate the log-odd values
+    myMap = min(myMap,lo_max);
+    myMap = max(myMap,lo_min);
+    
+    % Visualize the map as needed
+    robotGrid = [robotGrid; car_id];
+    imagesc(myMap); hold on;
+    plot(robotGrid(:,1),robotGrid(:,2),'r.-','LineWidth',2.5); % indicate start point
+    axis equal;
+    lidar_global(:,1) =  (ranges(:,j).*cos(scanAngles + pose(3,j)) + pose(1,j))*myResol + myorigin(1);
+    lidar_global(:,2) = (-ranges(:,j).*sin(scanAngles + pose(3,j)) + pose(2,j))*myResol + myorigin(2);
+    plot(lidar_global(:,1), lidar_global(:,2), 'g.'); 
+    hold off;
+    drawnow;    
 
 end
 
