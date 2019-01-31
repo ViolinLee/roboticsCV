5465eeeef9ad62993f4da876bd8756c2f6126861
ViolinLee <mechleechan@outlook.com>
2019-02-01 00:31:36 +0800

    fix sigh singularity in ar_cube.m

--- exercises/image_projection_simpleAR/ar_cube.m
+++ exercises/image_projection_simpleAR/ar_cube.m
@@ -11,8 +11,30 @@ function [proj_points, t, R] = ar_cube(H,render_points,K)
 %    R - size (3 x 3) matrix of the rotation of the transformation
 % Written by Stephen Phillips for the Coursera Robotics:Perception course
 
-% YOUR CODE HERE: Extract the pose from the homography
+% YOUR CODE HERE: Extract the pose from the estimated homography (H)
+if H(3,3) < 0
+    H = -H;
+end
+
+h1 = H(:,1);
+h2 = H(:,2);
+h3 = cross(h1, h2);
+
+[U,S,V] = svd([h1 h2 h3]);
+R = U * [1 0 0; 0 1 0; 0 0 det(U*V')] * V';
+t = H(:,3) / norm(h1);
 
 % YOUR CODE HERE: Project the points using the pose
+% m is the # of corners to describe correspondence
+n = size(render_points, 1);
+
+proj_points = zeros(n,2);
+
+% For each video point, calculate the corresponding logo point
+for i=1:n
+    proj_pts_vec = K * (R * [render_points(i,1); render_points(i,2); render_points(i,3)]+t);
+    proj_points(i,1) = proj_pts_vec(1)/proj_pts_vec(3);
+    proj_points(i,2) = proj_pts_vec(2)/proj_pts_vec(3);
+end
 
 end
