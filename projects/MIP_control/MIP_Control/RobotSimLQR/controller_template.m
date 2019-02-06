4e2a61a1ac732167df355de97bb2fce40b6e5458
ViolinLee <mechleechan@outlook.com>
2019-02-06 09:26:38 +0800

    arrange file

--- projects/MIP_control/MIP_Control/RobotSimLQR/controller_template.m
+++
@@ -1,9 +0,0 @@
-
-function u = controller(params, t, X)
-  % You have full state feedback available
-  u=0;
-
-  % After doing the steps in simLinearization, you should be able to substitute the linear controller u = -K*x
-  
-end
-
